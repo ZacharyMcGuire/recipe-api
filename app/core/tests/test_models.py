495da4560f1bc25f2b9723482069eb9fd3165ff3
Zachary McGuire <zacharysjmcguire@gmail.com>
2020-03-23 16:46:41 +1100

    normalized email addresses for the User model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,3 +16,12 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """
+        Test the email for a new user is normalized
+        """
+        email = 'test@ExAmPlE.coM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
